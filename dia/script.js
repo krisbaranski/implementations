34f093f2b9f2b60ff9caa636139efdcfc393a026
Kris Baranski <krisbaranski@gmail.com>
2021-03-03 14:29:46 +0100

    dia site progress

--- dia/script.js
+++ dia/script.js
@@ -1,4 +1,42 @@
-"use strict";
+'use strict';
 
-const dia = document.querySelectorAll('.dia');
-const 
+const dias = document.querySelectorAll('.dia');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let currentActive = 0;
+
+prev.addEventListener('click', () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 0;
+  }
+  update();
+});
+
+next.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > dias.length - 1) {
+    currentActive = dias.length - 1;
+  }
+  update();
+});
+
+function update() {
+  dias.forEach((dia, idx) => {
+    if (idx < currentActive || idx > currentActive) {
+      dia.classList.remove('active');
+    } else {
+      dia.classList.add('active');
+    }
+  });
+
+  if (currentActive === 0) {
+    prev.disabled = true;
+  } else if (currentActive === dias.length - 1) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
