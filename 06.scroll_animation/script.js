a8ccd8b32cf7cabd978c6374fb8bf760799eec60
Kris Baranski <krisbaranski@gmail.com>
2021-03-05 12:09:49 +0100

    made scroll animation

---
+++ 06.scroll_animation/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+}
+// you can use jQuery or animateOnScroll instead
