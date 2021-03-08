5b624e81b3a2ac32ccce69ccd4a0b6805254ca81
Kris Baranski <krisbaranski@gmail.com>
2021-03-08 11:45:30 +0100

    login animation wave finished

--- 08.login_wave_animation/script.js
+++ 08.login_wave_animation/script.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join('');
+});
