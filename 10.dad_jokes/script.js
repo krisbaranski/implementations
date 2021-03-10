acbbaec96f626bf83532061a415a1f466833c6fe
Kris Baranski <krisbaranski@gmail.com>
2021-03-10 11:11:56 +0100

    update html css

--- 10.dad_jokes/script.js
+++ 10.dad_jokes/script.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+const jokeEl = document.getElementByID('joke');
+const jokeBtn = document.getElementByID('jokeBtn');
+
+generateJoke();
+
+function generateJoke() {
+  fetch('https://icanhazdadjoke.com');
+}
