7ec8fbe0b51008b1fbff16303097f4670212a31e
Kris Baranski <krisbaranski@gmail.com>
2021-03-15 23:12:43 +0100

    begin random choice picker

--- 13.random_choice/script.js
+++ 13.random_choice/script.js
@@ -4,3 +4,23 @@ const tagsEl = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
 textarea.focus();
+
+textarea.addEventListener('keyup', e => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter(tag => tag.trim() !== '')
+    .map(tag => tag.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEL.innerText = tag;
+    tagsEl.appendChild(tagEL);
+  });
+}
