7bcaffcb66e1db9570501cb0018d0d3c0debffc6
Kris Baranski <krisbaranski@gmail.com>
2021-03-18 18:32:06 +0100

    finished

--- 13.random_choice/script.js
+++ 13.random_choice/script.js
@@ -7,6 +7,13 @@ textarea.focus();
 
 textarea.addEventListener('keyup', e => {
   createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ' ';
+    }, 10);
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -23,4 +30,41 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unhighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    });
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unhighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
