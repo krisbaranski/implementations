8e932a3434ef739524e9d77608c1fd9b577e3a1e
Krzysztof Baranski <krisbaranski@gmail.com>
2021-03-25 17:48:45 +0100

    changed style of progress line

--- 14.animated_navigation/script.js
+++ 14.animated_navigation/script.js
@@ -3,6 +3,4 @@
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
 
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
+toggle.addEventListener('click', () => nav.classList.toggle('active'));
