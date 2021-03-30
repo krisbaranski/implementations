bc20efc937c5bf58464b5fb4316ec1c3753d6b1f
Krzysztof Baranski <krisbaranski@gmail.com>
2021-03-30 23:13:02 +0200

    drink water
    add functionality to cups

--- 16.drink_water/script.js
+++ 16.drink_water/script.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict';
+
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+  smallCups.forEach(cup, idx2) => {
+      
+  }
+}
