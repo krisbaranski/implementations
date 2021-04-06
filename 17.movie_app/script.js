1c203a9248a37d4e52d2c0ae950e5e82e9a3eb8d
Krzysztof Baranski <krisbaranski@gmail.com>
2021-04-06 21:24:14 +0200

    movie_app
    async/await from MoviesAPI, search value

--- 17.movie_app/script.js
+++ 17.movie_app/script.js
@@ -1,1 +1,35 @@
-"use strict";
+'use strict';
+
+const API_URL =
+  'https:/api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=25abe828f8680becf698c08e24be0b51&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_API =
+  'https:/api.themoviedb.org/3/search/movie?api_key=25abe828f8680becf698c08e24be0b51&query="';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+// Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.results);
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
