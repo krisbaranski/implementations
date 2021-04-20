ec709d1055fc1c31712ce5e206dcd113e2f7278e
Krzysztof Baranski <krisbaranski@gmail.com>
2021-04-20 18:35:16 +0200

    defining drawLine and drawCircle for the canvas

--- 22.drawing_pad/script.js
+++ 22.drawing_pad/script.js
@@ -1,1 +1,25 @@
-"use strict";
+'use strict';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let size = 20;
+let color = 'black';
+let x;
+let y;
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2, true);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
