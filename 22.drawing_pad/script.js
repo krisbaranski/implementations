04e25b6f6fe658f3009903420024212467d77541
Krzysztof Baranski <krisbaranski@gmail.com>
2021-04-21 12:40:19 +0200

    add func to buttons, func for drawing

--- 22.drawing_pad/script.js
+++ 22.drawing_pad/script.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+let size = 2;
+let isPressed = false;
 let color = 'black';
 let x;
 let y;
@@ -20,6 +27,63 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size;
+}
+
+canvas.addEventListener('mousedown', e => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+
+  console.log(x, y);
+});
+
+canvas.addEventListener('mousemove', e => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
+
+canvas.addEventListener('mouseup', e => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+increaseBtn.addEventListener('click', () => {
+  size += 2;
+
+  if (size > 50) {
+    size = 50;
+  }
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 2;
+
+  if (size < 2) {
+    size = 2;
+  }
+  updateSizeOnScreen();
+});
+
+colorEl.addEventListener('change', e => (color = e.target.value));
+
+clearEl.addEventListener('click', () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
