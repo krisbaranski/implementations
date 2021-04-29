0e1677ed04fa098a050721105e3e1d5bfacbdf09
Krzysztof Baranski <krisbaranski@gmail.com>
2021-04-29 09:23:04 +0200

    header getData input, animated class

--- 24.content_placeholder/script.js
+++ 24.content_placeholder/script.js
@@ -1,1 +1,28 @@
-"use strict";
+'use strict';
+
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80" alt="" />';
+  title.innerHTML = 'Lorem ipsum dolor sit amet.';
+  excerpt.innerHTML =
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio cupiditate.';
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
+  name.innerHTML = 'John Doe';
+  date.innerHTML = 'Oct 20, 2020';
+
+  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+  animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'));
+}
