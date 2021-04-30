a15dc023f88d39485096648840f2cd9cda55feb6
Krzysztof Baranski <krisbaranski@gmail.com>
2021-04-30 14:18:34 +0200

    function fixNav, class active

--- 25.sticky_navbar/script.js
+++ 25.sticky_navbar/script.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+}
