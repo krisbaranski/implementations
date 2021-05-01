600c32a211d7a6166ac38c765ff9ffe958b7ce9b
Krzysztof Baranski <krisbaranski@gmail.com>
2021-05-01 12:03:11 +0200

    btn functionality, random notification and type

---
+++ 27.toast_notification/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
+
+const messages = [
+  'Message',
+  'Another Message',
+  'Message Three',
+  'Something went wrong',
+];
+
+const types = ['info', 'success', 'error'];
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div');
+
+  notif.classList.add('toast');
+  notif.classList.add(type ? type : getRandomType());
+  notif.innerText = message ? message : getRandomMessage();
+
+  toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 4000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
+}
