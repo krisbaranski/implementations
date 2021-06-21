6c0235673dc96243cd4e98e0cde45d6259dc0095
Krzysztof Baranski <krisbaranski@gmail.com>
2021-06-21 14:06:04 +0200

    function dblclick, click positioning, click increment, remove element setTimeout

--- course/29.double_heart_click/script.js
+++ course/29.double_heart_click/script.js
@@ -1,1 +1,44 @@
-"use strict";
+'use strict';
+
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let clickTime = 0;
+let timesClicked = 0;
+
+const createHeart = e => {
+  const heart = document.createElement('i');
+  heart.classList.add('fas');
+  heart.classList.add('fa-heart');
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const offsetLeft = e.target.offsetLeft;
+  const offsetTop = e.target.offsetTop;
+
+  const xInside = x - offsetLeft;
+  const yInside = y - offsetTop;
+
+  heart.style.left = `${xInside}px`;
+  heart.style.top = `${yInside}px`;
+
+  loveMe.appendChild(heart);
+
+  times.innerHTML = ++timesClicked;
+
+  setTimeout(() => heart.remove(), 1000);
+};
+
+loveMe.addEventListener('click', e => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      createHeart(e);
+      clickTime = 0;
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
+});
