374dd8e332f4cd56a75ef05642c532fac6e666be
Krzysztof Baranski <krisbaranski@gmail.com>
2021-06-24 22:57:47 +0200

    functions generate letters, numbers, symbols, functions for checked boxes

--- course/31.password_generator/script.js
+++ course/31.password_generator/script.js
@@ -1,4 +1,12 @@
 'use strict';
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
 const randomFunc = {
   lower: getRandomLower,
@@ -7,12 +15,35 @@ const randomFunc = {
   symbol: getRandomSymbol,
 };
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function generatePassword(upper, lower, number, symbol) {
+  let generatedPassword = '';
+  const typesCount = upper + lower + number + symbol;
+  const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(
+    item => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      console.log(type);
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
 }
 
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomNumber() {
@@ -23,3 +54,19 @@ function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasUpper = uppercaseEl.checked;
+  const hasLower = lowercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasUpper,
+    hasLower,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
