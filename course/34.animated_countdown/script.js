2d83494ed5fbe790c649cd7b9e68af6903774d37
Krzysztof Baranski <krisbaranski@gmail.com>
2021-07-20 23:02:06 +0200

    runAnimation(), resetDOM() functions

--- course/34.animated_countdown/script.js
+++ course/34.animated_countdown/script.js
@@ -1,1 +1,43 @@
-"use strict";
+'use strict';
+
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const finalMessage = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    console.log(num, idx);
+    const nextToLast = nums.length - 1;
+
+    num.addEventListener('animationend', e => {
+      if (e.animationName === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in');
+        num.classList.add('out');
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in');
+      } else {
+        counter.classList.add('hide');
+        finalMessage.classList.add('show');
+      }
+    });
+  });
+}
+
+runAnimation();
+
+function resetDOM() {
+  counter.classList.remove('hide');
+  finalMessage.classList.remove('show');
+
+  nums.forEach(num => {
+    num.classList.value = '';
+
+    nums[0].classList.add('in');
+  });
+}
+
+replay.addEventListener('click', () => {
+  resetDOM();
+  runAnimation();
+});
