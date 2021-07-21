5e6e9bc8c3f3e9a33bb1c33d4ef75dc9c7a9aefb
Krzysztof Baranski <krisbaranski@gmail.com>
2021-07-21 22:22:25 +0200

    run interval, change img, reset interval, buttons EventListener

--- course/35.image_carousel/script.js
+++ course/35.image_carousel/script.js
@@ -1,1 +1,42 @@
-"use strict";
+'use strict';
+
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+let interval = setInterval(run, 3000);
+
+function run() {
+  idx++;
+  changeImg();
+}
+
+function changeImg() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++;
+  changeImg();
+  resetInterval();
+});
+
+leftBtn.addEventListener('click', () => {
+  idx--;
+  changeImg();
+  resetInterval();
+});
