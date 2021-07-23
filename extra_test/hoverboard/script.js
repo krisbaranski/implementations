f595fd63c58e4410b8a0d795e58bee4048713f34
Krzysztof Baranski <krisbaranski@gmail.com>
2021-07-23 11:42:44 +0200

    initial files

---
+++ extra_test/hoverboard/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const container = document.getElementById('container');
+const colors = ['turquoise', '#2ecc71', '#f6e58d', '#eb4d4b'];
+const squares = 10000;
+
+for (let i = 0; i < squares; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+// function removeColor(element) {
+//   element.style.background = '#1d1d1d';
+//   element.style.boxShadow = '0 0 2px #000';
+// }
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
