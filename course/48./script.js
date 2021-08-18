607abb4665825e685afd7fd2b8b8d404aceca417
Krzysztof Baranski <krisbaranski@gmail.com>
2021-08-18 20:48:25 +0200

    unsplash random images, flexbox rows, random images function

--- course/48./script.js
+++ course/48./script.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+const container = document.querySelector('.container');
+const unsplashUrl = 'https://source.unsplash.com/random/';
+const rows = 10;
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement('img');
+  img.src = `${unsplashUrl}${getRandomSize()}`;
+  container.appendChild(img);
+}
+
+function getRandomSize() {
+  return `${getRandomNr()}x${getRandomNr()}`;
+}
+
+function getRandomNr() {
+  return Math.floor(Math.random() * 10) + 300;
+}
