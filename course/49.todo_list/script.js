2f8461de440527bef4e716aa884dedfbcde33202
Krzysztof Baranski <krisbaranski@gmail.com>
2021-08-19 23:44:10 +0200

    add, remove, completed todo, local storage update

--- course/49.todo_list/script.js
+++ course/49.todo_list/script.js
@@ -1,1 +1,68 @@
-"use strict";
+'use strict';
+
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+  todos.forEach(todo => addTodo(todo));
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement('li');
+
+    if (todo && todo.completed) {
+      todoEl.classList.add('completed');
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener('click', () => {
+      todoEl.classList.toggle('completed');
+      updateLS();
+    });
+
+    todoEl.addEventListener('contextmenu', e => {
+      e.preventDefault();
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl);
+
+    input.value = '';
+  }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll('li');
+
+  const todos = [];
+  todosEl.forEach(todoEL => {
+    todo.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed'),
+    });
+  });
+}
+localStorage.setItem('todos', JSON.stringify(todos));
+
+// Setting and Getting Items to and from Local Storage
+
+// localStorage.setItem('name', JSON.stringify(obj));
+// JSON.parse(localStorage.getItem(obj));
